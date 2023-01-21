b175757b92b99e03093c323589b4b14e96ac8070
david graff <deg711@g.harvard.edu>
2023-01-21 18:41:54 -0500

    clean up hf code

--- pcmr/chemberta.py
+++ pcmr/chemberta.py
@@ -3,26 +3,23 @@ from pathlib import Path
 
 import torch
 from transformers import pipeline
-import selfies as sf
 
 from pcmr.data.molnet import get_task_data
 from pcmr.rogi import rogi
 from pcmr.utils import Dataset, Metric
 
-CHEMGPT =  "ncfrey/ChemGPT-1.2B"
+CHEMBERTA = "DeepChem/ChemBERTa-77M-MLM"
 
 
 def main(args):
     featurizer = pipeline(
-        "feature-extraction", model=CHEMGPT, device=0, framework="pt", return_tensors=True
+        "feature-extraction", model=CHEMBERTA, device=0, framework="pt", return_tensors=True
     )
-    featurizer.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
     smis, y = get_task_data(args)
 
-    sfs = [sf.encoder(smi) for smi in smis]
-    output = featurizer(sfs)
-    X = torch.stack([H[0, -1, :] for H in output]).numpy()
+    output = featurizer(smis, batch_size=args.batch_size)
+    X = torch.stack([H[0, 0, :] for H in output]).numpy()
 
     score, _ = rogi(y, True, X, metric=Metric.EUCLIDEAN)
     print(score)
@@ -32,8 +29,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset", choices=Dataset.keys())
     parser.add_argument("-t", "--task")
-    parser.add_argument("-o", "--output", type=Path)
-    parser.add_argument("-b", "--batch-size", type=int, default=8)
+    parser.add_argument("-o", "--output-dir", type=Path)
+    parser.add_argument("-b", "--batch-size", type=int, default=32)
 
     args = parser.parse_args()
     args.dataset = Dataset.get(args.dataset)
