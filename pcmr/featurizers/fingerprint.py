14dcde5efa44ddeaf8a366f6c7f62483677cb068
david graff <deg711@g.harvard.edu>
2023-01-24 17:30:17 -0500

    fingeprint featurizer scaffold

---
+++ pcmr/featurizers/fingerprint.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+import numpy as np
+from rdkit import Chem
+
+from pcmr.featurizers.base import FeaturizerBase, FeaturizerRegistry
+
+
+# @FeaturizerRegistry.register(alias="morgan")
+# class MorganFeauturizer(FeaturizerBase):
+#     def __init__(self, radius: int = 2, length: int = 2048):
+#         self.radius = radius
+#         self.length = length
+
+#     def __call__(self, smis: Iterable[str]) -> np.ndarray:
+#         mols = [Chem.MolFromSmiles(smi) for smi in smis]
+#         return [
+#             Chem.GetMorganFingerprintAsBitVect(m, radius=self.radius, nBits=self.length)
+#             for m in mols
+#         ]
