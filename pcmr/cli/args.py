0121ce0feea10e88b1e7d69fa77fbdf05d9fe9b4
david graff <deg711@g.harvard.edu>
2023-01-26 10:06:17 -0500

    reorg cli

--- pcmr/cli/args.py
+++ pcmr/cli/args.py
@@ -1,18 +1,9 @@
 from argparse import ArgumentError, ArgumentParser, Action, Namespace
-from dataclasses import dataclass, asdict
-from itertools import repeat
-import logging
 from pathlib import Path
-from typing import Iterable, NamedTuple, Optional, Sequence, Union
-
-import numpy as np
-import pandas as pd
+from typing import Iterable, Optional, Sequence, Union
 
 from pcmr.data import data
 from pcmr.featurizers import FeaturizerRegistry
-from pcmr.featurizers.base import FeaturizerBase
-from pcmr.rogi import rogi
-from pcmr.utils import Metric
 
 
 class DatasetAndTaskAction(Action):
@@ -41,6 +32,7 @@ class DatasetAndTaskAction(Action):
 
     def parse_value(self, value: str):
         tokens = value.split('/')
+
         if len(tokens) == 1:
             dataset = tokens[0]
             task = None
@@ -53,20 +45,13 @@ class DatasetAndTaskAction(Action):
         task = task or None
         if self._choices and dataset not in self._choices:
             raise ArgumentError(self, f"invalid choice: 'dataset' must one of {self._choices}")
-        return dataset,task
-
-
-# @dataclass
-class RogiCalculationResult(NamedTuple):
-    featurizer: str
-    dataset: str
-    task: Optional[str]
-    n_valid: int
-    rogi: float
+        
+        return dataset, task
 
 
 def build_parser():
     parser = ArgumentParser()
+    
     parser.add_argument(
         "-f", "--featurizers",
         type=lambda s: s.lower(), nargs="+", choices=FeaturizerRegistry.keys()
@@ -83,49 +68,3 @@ def build_parser():
     parser.add_argument("-b", "--batch-size", type=int, default=8)
 
     return parser
-
-
-def calc_rogi(
-    featurizer: str, dataset: str, task: Optional[str], N: int, batch_size: int, repeats: int
-) -> list[RogiCalculationResult]:
-    f: FeaturizerBase = FeaturizerRegistry[featurizer]()
-    df = data.get_data(dataset, task, N)
-
-    X = f(df.smiles.tolist())
-    score, _ = rogi(df.y.tolist(), True, X, metric=Metric.EUCLIDEAN, min_dt=0.01)
-    n_valid = len(X) - np.isnan(X).any(1).sum(0)
-
-    result = RogiCalculationResult(featurizer, dataset, task, n_valid, score)
-    if len(df) < N:
-        results = list(repeat(result, repeats))
-    else:
-        results = [result]
-        for _ in range(repeats - 1):
-            df = data.get_data(dataset, task, N)
-
-            X = f(df.smiles.tolist())
-            score, _ = rogi(df.y.tolist(), True, X, metric=Metric.EUCLIDEAN, min_dt=0.01)
-            n_valid = len(X) - np.isnan(X).any(1).sum(0)
-            results.append(RogiCalculationResult(featurizer, dataset, task, n_valid, score))
-
-    return results
-
-
-def main():
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s:%(name)s - %(message)s', level=logging.INFO
-    )
-
-    parser = build_parser()
-    args = parser.parse_args()
-    print(args)
-
-    rows = []
-    for featurizer in args.featurizers:
-        for dataset, task in args.datasets_tasks:
-            results = calc_rogi(featurizer, dataset, task, args.N, args.batch_size, args.repeats)
-            rows.extend(results)
-
-    df = pd.DataFrame(rows)
-    print(df)
-    df.to_csv(args.output, index=False)
---
+++ pcmr/cli/utils.py
@@ -0,0 +1,10 @@
+
+from typing import NamedTuple, Optional
+
+
+class RogiCalculationResult(NamedTuple):
+    featurizer: str
+    dataset: str
+    task: Optional[str]
+    n_valid: int
+    rogi: float
