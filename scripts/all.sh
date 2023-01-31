83fd05f45acbbc7397675e11eec4d6176333f1d1
david graff <deg711@g.harvard.edu>
2023-01-31 15:54:00 -0500

    ignore a bunch of files

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,8 +1,11 @@
 output=$1
 input=${2:-scripts/datasets.all.txt}
-featurizers=${3:-'chemberta descriptor'}
+featurizers=${3:-'chemberta'}
 N=${4:-10000}
 repeats=${5:-5}
 
+logfile=logs/`date +%FT%H:%M:%S`.log
+mkdir -p `dirname $logfile`
+echo $logfile
 
-pcmr rogi -o $output -f ${featurizers} -N$N --input ${input} -r $repeats
+pcmr -vvvv --log $logfile rogi -o $output -f ${featurizers} -N$N --input ${input} -r $repeats
