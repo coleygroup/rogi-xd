b9c9369094efc553ec39fd615848080a02239a40
david graff <deg711@g.harvard.edu>
2023-02-09 17:45:10 -0500

    reorganize

--- scripts/rogi.sh
+++ scripts/rogi.sh
@@ -1,17 +1,19 @@
 #/bin/bash
 featurizers=$1
-input=${2:-scripts/datasets.all.txt}
+input=${2:-scripts/tdc.txt}
 N=${3:-10000}
 repeats=${4:-5}
 
 if [ -z "$featurizers" ]; then
-    featurizers=( "descriptor chemberta chemgpt GIN VAE" )
+    featurizers=( descriptor chemberta chemgpt GIN VAE )
 else
     featurizers=( $featurizers )
 fi
 
+echo "Running with featurizers: ${featurizers[@]// /, }"
+
 for f in "${featurizers[@]}"; do
-    output=results/raw/${f}.csv
+    output=results/raw/`basename $input .txt`/${f}.csv
     model_dir=models/${f}/zinc
     pcmr rogi -i ${input} -o $output -f $f -N$N -r $repeats -m ${model_dir} -vvvv --log
 done
